1390de6940cb75c93020fcaab256dcee89c850dc
Lilia Ordinyan <101642325+ordinyanlilia@users.noreply.github.com>
2025-12-12 11:57:26 +0400

    Create Task.js

---
+++ task-manager-service/models/Task.js
@@ -0,0 +1,42 @@
+// src/models/Task.js
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    index: true
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  dueDate: {
+    type: Date
+  },
+  priority: {
+    type: String,
+    enum: ['low', 'medium', 'high'],
+    default: 'medium'
+  },
+  status: {
+    type: String,
+    enum: ['to-do', 'in-progress', 'completed'],
+    default: 'to-do'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
